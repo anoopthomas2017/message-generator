7f0ad78db4e092bd0762cfffb73d9fa803f6f620
anoopthomas2017 <anooptho4u@gmail.com>
2023-05-13 15:05:52 +0530

    finised

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 //alert("hai")
+
+const subs = ['Anoop', 'Bibin', 'Joji', 'Alan']
+const verbs = ['likes', 'hates', 'loves', 'eats']
+const objs = ['apples', 'bananas', 'chocolates', 'dates']
 function generateMessage() {
-  console.log("new message");
+  //console.log("new message");
+  let ranSub = subs[Math.floor(Math.random() * subs.length)]
+  let ranVerb = verbs[Math.floor(Math.random() * verbs.length)]
+  let ranObjs = objs[Math.floor(Math.random() * objs.length)]
+
+  let msg = ranSub + ' ' + ranVerb + ' ' + ranObjs + '.'
+  console.log(msg)
+  document.getElementById('msg').textContent = msg;
 }
+generateMessage()
